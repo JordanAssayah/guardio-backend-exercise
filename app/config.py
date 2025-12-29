2bf1dd3f7dcd2655e688313fb910696c9a080044
Jordan Assayah <jordanassayah@gmail.com>
2025-12-29 17:12:32 +0200

    Add AppConfig class with lru_cache for single instance retreival of env vars

---
+++ app/config.py
@@ -0,0 +1,21 @@
+"""
+Application configuration from environment variables.
+"""
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+
+class AppConfig(BaseSettings):
+    pokeproxy_config: str
+    pokeproxy_secret: str
+    # Max body size in bytes. Default 4KB (~40x larger than a typical Pokemon message)
+    pokeproxy_max_body_size: int = 4096
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+@lru_cache
+def get_config() -> AppConfig:
+    return AppConfig()
